bda95f09304a605662e8bad79633d71631b63635
MexTermin <astronboil.p@hotmail.com>
2021-11-02 20:24:04 -0400

    end queue

--- EstructurasDeDatos/Colas/ArrayQueue.go
+++ EstructurasDeDatos/Colas/ArrayQueue.go
@@ -3,24 +3,22 @@ package EstructurasDeDatos
 import utils "algoritmos/EstructurasDeDatos/Utils"
 
 type ArrayQueue struct {
-	children []interface{}
+	children     []interface{}
+	lastPosition int
 }
 
 // O(1)
 func (Queue *ArrayQueue) IsEmpty() bool {
-	return Queue.children[0] == nil // 1
+	return Queue.lastPosition == 0 // 1
 }
 
 // O(n)
 func (Queue *ArrayQueue) EnQueue(value interface{}) {
-	if Queue.children[len(Queue.children)-1] != nil { // 1
+	if Queue.lastPosition == len(Queue.children) { // 1
 		Queue.children = utils.ResizeArray(Queue.children, len(Queue.children)*2) // n
 	}
-	var position int = 0                  // 1
-	for Queue.children[position] != nil { // n
-		position++ // 1
-	}
-	Queue.children[position] = value // 1
+	Queue.children[Queue.lastPosition] = value // 1
+	Queue.lastPosition++                       // 1
 }
 
 // O(n)
@@ -37,7 +35,8 @@ func (Queue *ArrayQueue) DeQueue() interface{} {
 	if Queue.children[(len(Queue.children)/2)+1] != nil { // 1
 		Queue.children = utils.ResizeArray(Queue.children, len(Queue.children)*2) // n
 	}
-	return result // 1
+	Queue.lastPosition-- // 1
+	return result        // 1
 }
 
 // O(1)
@@ -53,22 +52,15 @@ func (Queue *ArrayQueue) Tail() interface{} {
 	if Queue.IsEmpty() { // 1
 		panic("La cola esta vacia") // 1
 	}
-	if Queue.children[len(Queue.children)-1] != nil { // 1
-		return Queue.children[len(Queue.children)-1] // 1
-	}
-
-	var position int = 0                  // 1
-	for Queue.children[position] != nil { // n
-		position++ // 1
-	}
-	return Queue.children[position-1] // 1
+	return Queue.children[Queue.lastPosition-1] // 1
 }
 
 //-------------Instanciador del Queue, y enlace de esta con la interfaz IQueue
 // O(1)
 func NewArrayQueue() *ArrayQueue {
 	var ArrayQueue *ArrayQueue = &ArrayQueue{ // 1
-		children: make([]interface{}, 10), // 1
+		children:     make([]interface{}, 10), // 1
+		lastPosition: 0,
 	}
 	implementIQueue(ArrayQueue) // 1
 	return ArrayQueue           // 1
